cd3b1304a47412a96ac9ccbda0cca5cabbf47fdf
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-06-02 17:43:40 +0200

    Update main.sh

--- scripts/bash/main.sh
+++ scripts/bash/main.sh
@@ -2,18 +2,13 @@ INPUT=inputs/inputs.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; }
-LIMIT=15
 
 while read application package
 do
 	echo "application : $application"
 	echo "package : $package"
 
+    ls "bins/$application" 
+  java -d64 -Xmx10000m  -jar libs/model-generation.jar -project_cp "bins/$application" -project_prefix "$package" -out_dir "models/$application" > "logs/$application-out.txt" 2> "logs/$application-err.txt" 
 
-  java -d64 -Xmx10000m -jar libs/model-generation.jar -project_cp "bins/$application" -project_prefix "$package" -out_dir "models/$application" > "logs/$application-out.txt" 2> "logs/$application-err.txt" &
-
-  while (( $(pgrep -l java | wc -l) >= $LIMIT ))
-    do
-                sleep 1
-    done
 done < $INPUT
